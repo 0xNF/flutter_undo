28a66b7491a9735ab66a3c2b7c98c998254ffb25
0xnf <0xnf@wintech.com>
2023-09-12 16:18:12 +0900

    Changed out logger reference

--- lib/src/logger.dart
+++ lib/src/logger.dart
@@ -1,10 +1,10 @@
-import 'package:dart_ilogger/dart_ilogger.dart';
+import 'package:ilogger/ilogger.dart';
 
 /// public Hookpoint for attaching anything implementing an ILogger
 ILogger get logger => _logger;
-ILogger _logger = const BasicLogger(name: "Flutter_Undo_Logger");
+ILogger _logger = BasicConsoleLogger(name: "Flutter_Undo_Logger");
 
 /// Sets a Hooked Logger for the library. If `logger` is `null`, then no messages will be logged
 void setLoggerHook(ILogger? logger) {
-  _logger = logger ?? const NullLogger(name: "Flutter_Undo_Null_Logger");
+  _logger = logger ?? BasicNullLogger(name: "Flutter_Undo_Null_Logger");
 }
